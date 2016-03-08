e749edde8964b3eeae724d15da11bc472bf97ac4
davenpsj <davenpsj@uchicago.edu>
2016-03-08 13:15:31 -0600

    webapp

---
+++ webapp/dfdisplay.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+import pandas as pd
+app = Flask(__name__)
+
+
+import config
+
+
+df = pd.read_csv(config.file_to_display)
+
+
+@app.route('/')
+def load_dataframe():
+    cols = list(df.columns)
+    return render_template('dataframe.html', data=df.to_dict(), cols=cols, nrows=len(df))
+
+@app.route('/about')
+def about():
+    return render_template('about.html', name='Spencer Davenport')
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return '404!!!!'
+
+
+if __name__ == '__main__':
+    app.run()
